f035407481ae931e12f225c68f8de1e88279ab34
kev1226 <kevin_alt12@hotmail.com>
2025-06-25 13:22:44 -0500

    check port kafka

--- src/main/java/com/tecnoshop/searchProducts/config/KafkaConfig.java
+++ src/main/java/com/tecnoshop/searchProducts/config/KafkaConfig.java
@@ -15,7 +15,7 @@ public class KafkaConfig {
     @Bean
     public ProducerFactory<String, String> producerFactory() {
         Map<String, Object> config = new HashMap<>();
-        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "localhost:9092");
+        config.put(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, "3.232.44.31:9092");
         config.put(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         config.put(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class);
         return new DefaultKafkaProducerFactory<>(config);
